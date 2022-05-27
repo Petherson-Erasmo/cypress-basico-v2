4514a21ffc712d5ba03b643a6e568652b9489880
Petherson Erasmo da Silva <petherson_erasmo@hotmail.com>
2022-05-26 23:11:35 -0300

    test(session 4): select options in drop-down selection fields + exercise

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,16 +1,16 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', ()=> {
-    beforeEach(()=> {
+describe('Central de Atendimento ao Cliente TAT', () => {
+    beforeEach(() => {
         cy.visit('./src/index.html')
     })
-    
-    it('Verifica o título da aplicação', ()=> {
+
+    it('Verifica o título da aplicação', () => {
         cy.title()
             .should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it('Preenche os campos obrigatórios e envia o formulário', ()=> {
+    it('Preenche os campos obrigatórios e envia o formulário', () => {
         const text = "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss deixa as pessoas mais interessantis."
 
         cy.get('input[id="firstName"]')
@@ -20,14 +20,14 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
         cy.get('input[id="email"]')
             .type('test@teste.com')
         cy.get('div textarea[id="open-text-area"]')
-            .type(text, {delay:0})
+            .type(text, { delay: 0 })
         cy.get('button[class="button"]')
             .contains('Enviar')
             .click()
         cy.successMessage()
     })
 
-    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', ()=>{
+    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
         cy.get('input[id="firstName"]')
             .type("Teste")
         cy.get('input[id="lastName"]')
@@ -42,13 +42,13 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
         cy.erroMessage()
     })
 
-    it('Não deveria aceitar caracteres não númericos no campo telefone', ()=>{
+    it('Não deveria aceitar caracteres não númericos no campo telefone', () => {
         cy.get('input[id="phone"]')
             .type("Jane")
             .should('not.have.value', 'Jane')
     })
 
-    it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', ()=> {
+    it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
         cy.get('input[id="firstName"]')
             .type("Teste")
         cy.get('input[id="lastName"]')
@@ -65,7 +65,7 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
         cy.erroMessage()
     })
 
-    it('Preenche e limpa os campos nome, sobrenome, email e telefone', ()=> {
+    it('Preenche e limpa os campos nome, sobrenome, email e telefone', () => {
         cy.get('input[id="firstName"]')
             .type("Teste")
             .clear()
@@ -88,7 +88,7 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
         cy.erroMessage()
     })
 
-    it('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', ()=>{
+    it('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
         cy.get('button[class="button"]')
             .contains('Enviar')
             .click()
@@ -96,11 +96,11 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
             .should('be.visible')
     })
 
-    it('Envia o formuário com sucesso usando um comando customizado', ()=>{
+    it('Envia o formuário com sucesso usando um comando customizado', () => {
         cy.fillMandatoryFieldsAndSubmit()
     })
 
-    it('Substitui o get por contains', ()=>{
+    it('Substitui o get por contains', () => {
         const text = "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss deixa as pessoas mais interessantis."
 
         cy.get('input[id="firstName"]')
@@ -110,10 +110,34 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
         cy.get('input[id="email"]')
             .type('test@teste.com')
         cy.get('div textarea[id="open-text-area"]')
-            .type(text, {delay:0})
+            .type(text, { delay: 0 })
         cy.contains('button', 'Enviar')
             .click()
         cy.successMessage()
     })
 
+})
+
+describe.only('Central de Atendimento ao Cliente TAT - Sessão 4', () => {
+    beforeEach(() => {
+        cy.visit('./src/index.html')
+    })
+
+    it('Seleciona um produto (YouTube) por seu texto', () => {
+        cy.get('select[id="product"]')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('Seleciona um produto (Mentoria) por seu valor (value)', () => {
+        cy.get('select[id="product"]')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it('Seleciona um produto (Blog) por seu índice', () => {
+        cy.get('select[id="product"]')
+            .select(1)
+            .should('have.value', 'blog')
+    })
 })
